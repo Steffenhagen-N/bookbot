57cd2b74068a9537469c07d3b341b955db7d626f
Steffenhagen-N <nicholassteffenhagen@gmail.com>
2024-03-25 12:53:40 -0700

    rewrote text and wordcount functions to allow for cleaner iteration

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-
 def main():
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = get_word_count(text)
+    #character_count = 
+
+    print(word_count)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_word_count(text):
+    return len(text.split())
 
 main()
+
+#main()
 
 #I don't understand how any of this works
